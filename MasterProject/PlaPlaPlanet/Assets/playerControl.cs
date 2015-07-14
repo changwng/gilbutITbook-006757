4df6d6e0905f39a06e482f21503c1f2aec3c417a
Kimyuhan <ifpi98@gmail.com>
2015-07-15 03:03:48 +0900

    Add : jump game scene Rem : files do not track

---
+++ MasterProject/PlaPlaPlanet/Assets/playerControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+
+	private float power;
+	public float POWERPLUS = 100.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButton (0)) {
+			power += POWERPLUS * Time.deltaTime;
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			this.GetComponent<Rigidbody>().AddForce(new Vector3(power, power, 0));
+			power = 0.0f;
+		}
+
+		if (this.transform.position.y < -5.0f) {
+			Application.LoadLevel("gameScene");
+		}
+		   
+
+	}
+}
